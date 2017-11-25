2c0121c9e36c7d93773a8b6b8a27977ca9a44500
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-25 13:54:38 +0200

    Update gameloop.js

--- gameplay/battle/gameloop.js
+++ gameplay/battle/gameloop.js
@@ -67,6 +67,7 @@ window.onload = () => {
   const ctx = canvas.getContext('2d');
   var hitpointsGUI = new hpGUI(canvas, ctx);
   var weaponsGUI = new wpGUI(canvas, ctx);
+  var enemyTank = new EnemyTank(canvas, ctx);
   var mainTank;
   var shootAnimation = new Image(5121, 658);
 	shootAnimation.src = '../../graphics/shootAnimation(fixedRez).png';
@@ -92,7 +93,7 @@ window.onload = () => {
 		}
 
 		// draw enemy tank
-
+		enemyTank.drawTank();
 		// draw hp
 		hitpointsGUI.refreshHp();
 

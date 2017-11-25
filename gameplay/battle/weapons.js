859056d498364817a590a61ebab0974816fd465f
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-25 15:53:09 +0200

    Added weapon loading

--- gameplay/battle/weapons.js
+++ gameplay/battle/weapons.js
@@ -3,6 +3,7 @@ class wpGUI {
     this.canvas = canvas;
     this.ctx = ctx;
     this.isWeaponActive = false;
+    this.loadingBar = 0;
     this.weaponsPosition = {
       x0: 237,
       y0: 708,
@@ -11,8 +12,18 @@ class wpGUI {
     };
   }
 
-  drawWeapon() {
+  loadWeapon() {
+    this.loadingBar += 0.01;
+  }
 
+  depleteWeapon() {
+    this.loadingBar = 0;
+  }
+
+  drawWeapon() {
+    if (this.loadingBar > 0.98) {
+      this.isWeaponActive = true;
+    }
     if (this.isWeaponActive) {
       this.setActive();
       console.log("Sets active");
@@ -25,6 +36,8 @@ class wpGUI {
   setInactive() {
     this.ctx.fillStyle = 'red';
     this.ctx.fillRect(this.weaponsPosition.x0, this.weaponsPosition.y0, 120, 60);
+    this.ctx.fillStyle = 'yellow';
+    this.ctx.fillRect(this.weaponsPosition.x0, this.weaponsPosition.y0 + 60 - (this.loadingBar*60), 120, (this.loadingBar*60));
   }
 
   setActive() {

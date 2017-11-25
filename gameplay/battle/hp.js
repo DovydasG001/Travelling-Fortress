b21b0bbaedc2c6067940333b589dd6dd1ef7ccfb
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-25 11:55:01 +0200

    Update hp.js

--- gameplay/battle/hp.js
+++ gameplay/battle/hp.js
@@ -1,40 +1,42 @@
-var hpGUI = document.getElementById("hpGUI");
-var hpGUICtx = hpGUI.getContext("2d");
-var hitCount = 0;
-var hp = 10;
-var enemyHitCount = 0;
-var enemyHp = 10;
-var hpX= 75;
-
-hpGUICtx.font = "30px Arial";
-hpGUICtx.fillText("HP:", 10, 45);
-hpGUICtx.fillText("Enemy HP:", 600, 45);
-
-var drawBar = function (x){
-	hpGUICtx.beginPath();
-	hpGUICtx.lineWidth="3";
-	hpGUICtx.strokeStyle="black";
-	hpGUICtx.rect(x, 22, 10, 25);
-	hpGUICtx.fillStyle = "red";
-	hpGUICtx.fill();
-	hpGUICtx.stroke();
-	hpGUICtx.closePath();
-}
-
-var refreshHp = function(){
-	hpGUICtx.clearRect(75, 10, 230, 45);
-	hpGUICtx.clearRect(760, 10, 230, 45);
-	hpX= 75;
-	for (i=0; i <= hp - hitCount; i++){
-		drawBar(hpX);
-		hpX += 15;
+class hpGUI {
+	constructor(ctx, canvas) {
+		this.ctx = ctx;
+		this.canvas = canvas;
+		this.hitCount = 0;
+		this.hp = 10;
+		this.enemyHitCount = 0;
+		this.enemyHp = 10;
+		this.hpX= 75;
+		this.ctx.font = "30px Arial";
 	}
 
-	hpX = 760;
-	for (i=0; i <= enemyHp - enemyHitCount; i++){
-		drawBar(hpX);
-		hpX += 15;
+	drawBar(x) {
+		this.ctx.fillText("HP:", 10, 45);
+		this.ctx.fillText("Enemy HP:", 600, 45);
+		this.ctx.beginPath();
+		this.ctx.lineWidth="3";
+		this.ctx.strokeStyle="black";
+		this.ctx.rect(x, 22, 10, 25);
+		this.ctx.fillStyle = "red";
+		this.ctx.fill();
+		this.ctx.stroke();
+		this.ctx.closePath();
 	}
+
+	refreshHp() {
+		this.ctx.clearRect(75, 10, 230, 45);
+		this.ctx.clearRect(760, 10, 230, 45);
+		this.hpX = 75;
+		for (var i=0; i <= this.hp - this.hitCount; i++){
+			this.drawBar(this.hpX);
+			this.hpX += 15;
+		}
+
+		this.hpX = 760;
+		for (i=0; i <= this.enemyHp - this.enemyHitCount; i++){
+			this.drawBar(this.hpX);
+			this.hpX += 15;
+		}
+	}
+
 }
-
-

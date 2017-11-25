fab6260592fc5b497793875e7ef8665258a494bf
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-25 16:23:59 +0200

    Update mainTank.js

--- gameplay/battle/mainTank.js
+++ gameplay/battle/mainTank.js
@@ -6,8 +6,12 @@ class MainTank{
 		this.i = 0;
 		this.shootAnimation = shootAnimation;
 		this.shoot = false;
+		this.position = {
+			x: 0,
+			y: 50
+		};
 	}
-	
+
 	drawTank(){
 		if (this.shoot){
 			if( this.i == 32){
@@ -15,16 +19,16 @@ class MainTank{
 				this.sx = 0;
 				this.shoot = false;
 			}
-			this.ctx.drawImage(this.shootAnimation, this.sx, 0, 569, 658, 0, 150, 569, 658);
+			this.ctx.drawImage(this.shootAnimation, this.sx, 0, 569, 658, this.position.x, this.position.y, 569, 658);
 			this.i++;
 			if( this.i%4 == 0){
 				this.sx += 569;
 			}
-			
-			
+
+
 		} else {
-			this.ctx.drawImage(this.shootAnimation, 0, 0, 569, 658, 0, 150, 569, 658);
+			this.ctx.drawImage(this.shootAnimation, 0, 0, 569, 658, this.position.x, this.position.y, 569, 658);
 		}
 	}
-	
+
 }
